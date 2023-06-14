3560c4b925a7d7d74f07840ceb2f870d55c47475
ioangheraszim <gijohny17@gmail.com>
2023-06-14 23:16:58 +0100

    added card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Card from "./components/Card";
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <main className="container">
       <Navbar />
       <Hero />
+      <Card />
     </main>
   )
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Card() {
+    return (
+        <section className="card">
+            <img src="../src/assets/katie-zaferes.png" className="card-image" />
+            <div className="card-stats">
+                <img src="../src/assets/star.png" className="card-star"/>
+                <span>5.0</span>
+                <span className="grey">(6) • </span>
+                <span className="grey">USA</span>
+            </div>
+            <p>Life Lessons with Katie Zaferes</p>
+            <p>From $136</p>
+        </section>
+       
+    )
+}

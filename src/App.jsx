97ea17712869abe9f80b3a19df99d04eff22093b
ioangheraszim <gijohny17@gmail.com>
2023-06-18 17:54:30 +0100

    started using props

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,12 @@ import Hero from "./components/Hero";
 import Card from "./components/Card";
 
 function App() {
-
   return (
     <main className="container">
       <Navbar />
       <Hero />
       <Card 
-        img="./src/assets/katie-zaferes.png"
+        img="katie-zaferes.png"
         rating="5.0"
         reviewCount={6}
         country="USA"
